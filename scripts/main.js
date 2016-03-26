de8ed83870831f7f1399436bb907232394fd2e42
Paul Kirow <kirow1993@gmail.com>
2016-03-26 02:06:56 -0400

    fixed bug in button selecting

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
 var options = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
 var info = document.getElementsByClassName('info-container')[0].getElementsByTagName('div');
 
-function tabClick(e) {
+function tabClick(name) {
+  var target = null;
   // Remove active class from any classes that have it
   for (var i= 0; i<options.length; i++) {
     options[i].className='';
     info[i].className='';
+    if (options[i].href.split('#')[1] == name) {
+      target = options[i]
+    }
   }
-  // Add active class to clicked element
-  e = e || window.event;
-  var target = e.target || e.srcElement;
-  target = target.parentElement;
-  target.className = 'active';
-  // Add info class to info window
-  var targetId = target.href.split('#')[1];
-  document.getElementById(targetId).className = 'info';
+  if (target != null) {
+    // Add active class to clicked element
+    target.className = 'active';
+    // Add info class to info window
+    var targetId = target.href.split('#')[1];
+    document.getElementById(targetId).className = 'info';
+  }
 }
