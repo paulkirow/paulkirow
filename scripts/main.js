444bb130acdd9f7a1acc933c12f68626f666df00
Paul Kirow <kirow1993@gmail.com>
2016-03-26 01:17:51 -0400

    Further development on the menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 var options = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+var info = document.getElementsByClassName('info-container')[0].getElementsByTagName('div');
 
 function tabClick(e) {
   // Remove active class from any classes that have it
   for (var i= 0; i<options.length; i++) {
     options[i].className='';
+    info[i].className='';
   }
   // Add active class to clicked element
   e = e || window.event;
   var target = e.target || e.srcElement;
+  target = target.parentElement;
   target.className = 'active';
+  // Add info class to info window
+  var targetId = target.href.split('#')[1];
+  document.getElementById(targetId).className = 'info';
 }
